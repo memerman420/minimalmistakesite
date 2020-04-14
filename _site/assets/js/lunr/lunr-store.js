d5c94710b17b53f2af2d00b97e955966076b7cff
Aditya ARYA <ARY0004@students.nossalhs.vic.edu.au>
2020-04-14 17:39:59 +1000

    new changes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "An introduction",
         "excerpt":"Welcome to my blog, my introduction post is relating to how my brother is in fact a huge piece of defacation. Don’t you agree? Biggest stool = My brother I have deemed multiple reasons to why My brother has been given this classification. Abusing me for no apparent reason. Not...","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/sitedocs/An-introduction/",
+        "url": "http://localhost:4000/minimalmistakesite/sitedocs/An-introduction/",
         "teaser": null
       },{
         "title": "...",
         "excerpt":"be me     be me.   in quarantine.   have an idea to start a blog.   spent hours setting it up.   don’t know what to write.   why do I do this to myself.     ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/sitedocs/post/",
+        "url": "http://localhost:4000/minimalmistakesite/sitedocs/post/",
         "teaser": null
       },{
         "title": "A review - Deception Point - Dan Brown",
         "excerpt":"Reviewing Deception Point So I recently finished Deception Point by Dan Brown and like Dan Brown’s other novels, I have to say I’m impressed. This book features: Politics Ice A certain meteorite Presidency Deception point is one of two stand alone books that Dan Brown has written, the other being,...","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/sitedocs/review/",
+        "url": "http://localhost:4000/minimalmistakesite/sitedocs/review/",
         "teaser": null
       }]
