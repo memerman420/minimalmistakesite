ecd0b7318091f63fc99f3cfbabbb1de2ee7dcb46
Aditya ARYA <ARY0004@students.nossalhs.vic.edu.au>
2020-04-23 16:39:11 +1000

    r68

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,37 +1,37 @@
 var store = [{
         "title": "An introduction",
-        "excerpt":"Welcome to my blog, my introduction post is relating to how my brother is in fact a huge piece of defacation. Don’t you agree? Biggest stool = My brother I have deemed multiple reasons to why My brother has been given this classification. Abusing me for no apparent reason. Not...","categories": ["sitedocs"],
+        "excerpt":"Welcome to my blog, my introduction post is relating to how my brother is in fact a huge piece of defacation. Don’t you agree?   Biggest stool = My brother   I have deemed multiple reasons to why My brother has been given this classification.      Abusing me for no apparent reason.   Not being able to give proper instructions on how to solve a rubix cube.   Existing.   Holding the record for ugliest hairstyle in the world with no intention of giving it up.   Being short.   Singing on the toilet.   Reasons you should continue reading this nonce of a site.   You really shouldn’t actually, it’s a waste of brain cells. But by chance you do feel for some reason hooked:      There will be memes   Extreme low brow, degraded humour   No scheduling of posts and reviews among some other things   Don’t go here if you want to retain your sanity.   Things I enjoy doing when I have free time:     Anime (Yes i just do Anime)       Gaeming. I play a variety of games incuding: RPG’S, FPS, and motorsport games.       Sports. Cricket, Tennis and Swimming.    Download? What? There is nothing for you to download…   Like absolutely nothing   smacks lips   ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/An-introduction/",
+        "url": "http://localhost:4000/sitedocs/An-introduction/",
         "teaser": null
       },{
         "title": "...",
         "excerpt":"be me     be me.   in quarantine.   have an idea to start a blog.   spent hours setting it up.   don’t know what to write.   why do I do this to myself.  ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/post/",
+        "url": "http://localhost:4000/sitedocs/post/",
         "teaser": null
       },{
         "title": "A review - Deception Point - Dan Brown",
-        "excerpt":"Reviewing Deception Point So I recently finished Deception Point by Dan Brown and like Dan Brown’s other novels, I have to say I’m impressed. This book features: Politics Ice A certain meteorite Presidency Deception point is one of two stand alone books that Dan Brown has written, the other being,...","categories": ["sitedocs"],
+        "excerpt":"Reviewing Deception Point   So I recently finished Deception Point by Dan Brown and like Dan Brown’s other novels, I have to say I’m impressed. This book features:     Politics   Ice   A certain meteorite   Presidency   Deception point is one of two stand alone books that Dan Brown has written, the other being, Digital fortress. I love the little tid bits of real like knowledge that Dan brown seamlessly integrates into an action packed plot. For the majority of time I spent reading the book, I was confident I knew everything about the big reveal that was ineveitable. However, Dan Brown being Dan brown, turned my reading experience upside down with an extremely well though-out twist.   Characters  We are able to get relief from Robert Langdon and his constant reminders on how incredibly fit he is for his age. Meet Rachael Sexton, working for the current president while her dad attempts to remove the current president from his office by replacing him. Interesting situation right? It gets better if you read the book.   Conclusion/Thoughts  I really enoyed reasing this book and if you have enjoyed other Dan Brown novels, then I highly recommend this book to you. Much enjoyable, lots of suspense, and tips on how to win your very own presidential elction.   8/10     ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/review/",
+        "url": "http://localhost:4000/sitedocs/review/",
         "teaser": null
       },{
         "title": "Online School",
         "excerpt":"be me     Start online school   excitement.jpg   we have a system for something like this so we are “prepared”   It’s not the same   Would rather actually go to school   Surprise.exe   When will this end.exe   Serenity by Kupla  ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/lul/",
+        "url": "http://localhost:4000/sitedocs/lul/",
         "teaser": null
       },{
         "title": "Sites I use to download",
-        "excerpt":"Movies/T.V shows MoviesFlix Requires a bit of patience(3-5 redirects), but works perfectly fine, downloads fast and the site is updated very frequently. PopcornTime Go on the site, download the application. Set where you you would like to put your downloads. and ignore the warning about the VPN it gives you...","categories": ["sitedocs"],
+        "excerpt":"Movies/T.V shows     MoviesFlix    Requires a bit of patience(3-5 redirects), but works perfectly fine, downloads fast and the site is updated very frequently.     PopcornTime    Go on the site, download the application. Set where you you would like to put your downloads. and ignore the warning about the VPN it gives you (you can turn your vpn if you’re genuinely scared) and you’re done. I use this application when MoviesFlix doesn’t serve my needs. Also is updated very frequently(sometimes faster than MoviesFlix).   Anime     AnimeList    An okay site for downloading Anime, but there is not much to compare it to as it is one of the only sites that work for downlaoding anime. Would be an even better site but download speeds can vary a lot. Downloading episodes in batched can be really tiresome. I’m currently searching for a way to make this easier and I’ve found some promising results on Github. A certain program that runs on python and downloads episodes for you. I’ll update this when I’m certain it works.     Update:    After testing different ways on downloading anime, I’ve found a solution that works for me. Using Jdownloader paired with AnimeList works wonder for me. However, ther are times when animelist.pw cuases slow download speeds. But apart from those times, this method is excellent.   A note on the vpn  A reliable free vpn that connects to America. I would not reccommend this vpn for anything other than getting around blocks as it significantly reduces you network speed.  ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/sites/",
+        "url": "http://localhost:4000/sitedocs/sites/",
         "teaser": null
       },{
         "title": "We gaining",
-        "excerpt":"We gaining weight be me stuck inside like everyone else family starts cooking all sorts of foods much taste, very sublime at least something good is coming out of this but..but-Im gaining weight I’m underweight so it’s all good but it could get out of hand very quickly good thing...","categories": ["sitedocs"],
+        "excerpt":"We gaining weight     be me   stuck inside like everyone else   family starts cooking all sorts of foods       much taste, very sublime   at least something good is coming out of this   but..but-Im gaining weight   I’m underweight so it’s all good   but it could get out of hand very quickly   good thing I started working out a couple of months ago            Stay tuned for a workout routine and stay safe anons              ","categories": ["sitedocs"],
         "tags": [],
-        "url": "http://localhost:4000/minimalmistakesite/sitedocs/food/",
+        "url": "http://localhost:4000/sitedocs/food/",
         "teaser": null
       }]
